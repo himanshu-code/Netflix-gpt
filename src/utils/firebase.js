f1afd6d2ae94189f2fad187d5d5bb065f1602dcb
Himanshu Bhardwaj <himanshu.bhardwaj307@gmail.com>
2025-02-17 00:46:36 +0530

    Redux store

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -5,14 +5,7 @@ import { getAuth } from "firebase/auth";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDD2nfzFdeEg_tg3Go9QKlWEuH8kVu1Rwo",
-  authDomain: "netflixgpt-2d826.firebaseapp.com",
-  projectId: "netflixgpt-2d826",
-  storageBucket: "netflixgpt-2d826.firebasestorage.app",
-  messagingSenderId: "702067618433",
-  appId: "1:702067618433:web:b29b270dd58e5f6ab35380",
-};
+const firebaseConfig = {};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
